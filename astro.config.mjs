2bd9e0e5becbf73ecea7f90fea420c4951bdbcd2
Anirban Sikdar <anirban9293@gmail.com>
2026-01-04 23:00:39 +0530

    feat: update hardcoded domain to anirbansikdar.com and apply minor formatting adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://anirban-1009.vercel.app/",
+    site: "https://anirbansikdar.com/",
     base: "/",
     integrations: [tailwind(), icon(), mdx()],
     output: "server",
